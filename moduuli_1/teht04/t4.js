99e67d6cd9c4461b1fa03c0cd79aa766d1c7c99d
BenLarry <4muki4@gmail.com>
2025-11-04 19:04:04 +0200

    tehtävä 4 ratkaisu

--- moduuli_1/teht04/t4.js
+++ moduuli_1/teht04/t4.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const namee = prompt('Anna nimesi:');
+const num = Math.floor(Math.random() * 4);
+
+let faction = null;
+switch (num) {
+    case 0:
+        faction = 'Gryffindor';
+        break;
+
+    case 1:
+        faction = 'Slytherin';
+        break;
+
+    case 2:
+        faction = 'Hufflepuff';
+        break;
+
+    case 3:
+        faction = 'Ravenclaw';
+        break;
+
+    default:
+        faction = 'Factionless';
+        break;
+};
+
+document.querySelector('#target').innerText = `${namee}, you are ${faction}`;
