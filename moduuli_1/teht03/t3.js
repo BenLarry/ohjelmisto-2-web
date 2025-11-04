7a593ba4aa622969f3795930fdd7c1eec558602e
BenLarry <4muki@gmail.com>
2025-11-04 15:03:34 +0200

    tehtävä 3 ratkaisu

--- moduuli_1/teht03/t3.js
+++ moduuli_1/teht03/t3.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const num1 = +prompt('Enter number');
+const num2 = +prompt('Enter second number');
+const num3 = +prompt('Enter third number');
+
+const sum = num1 + num2 + num3;
+
+document.querySelector(
+  '#target'
+).innerText = `summa numeroista ${num1}, ${num2} ja ${num3} on ${sum}`;
