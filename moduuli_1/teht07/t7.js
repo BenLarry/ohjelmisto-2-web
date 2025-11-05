9c486e121b0ce25497f21d7b417ed088b4404b04
BenLarry <4muki@gmail.com>
2025-11-05 09:58:00 +0200

    comment removal

--- moduuli_1/teht07/t7.js
+++ moduuli_1/teht07/t7.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/*Write a program that rolls user defined number of dice and displays the sum of the results of the dice rolls.(2p)
-
-First, program asks the user for the number of dice rolls.
-Then the program throws a die as many times as the user defined.
-Print the sum of the results in the console or in the HTML document.*/
-
 const diceAmount = +prompt('Enter how many dices:');
 
 let total = 0;
