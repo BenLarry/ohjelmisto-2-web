6759dd02400b15b4af40fe96ec12dd52f9bd0690
BenLarry <4muki4@gmail.com>
2025-11-05 17:58:49 +0200

    .

--- moduuli_1/teht04/t4.js
+++ moduuli_1/teht04/t4.js
@@ -1,30 +1,6 @@
 'use strict';
 
-const name = prompt('Enter your name');
-
-const num = Math.floor(Math.random() * 4);
-
-let faction = null;
-switch (num) {
-  case 0:
-    faction = 'Gryffindor';
-    break;
-  case 1:
-    faction = 'Slytherin';
-    break;
-  case 2:
-    faction = 'Hufflepuff';
-    break;
-  case 3:
-    faction = 'Ravenclaw';
-    break;
-
-  default:
-    faction = 'factionless';
-    break;
-}
-
-document.querySelector('#target').innerText = `${name}, you are ${faction}`;
+'use strict';
 
 const name = prompt('Enter your name');
 
@@ -51,31 +27,3 @@ switch (num) {
 }
 
 document.querySelector('#target').innerText = `${name}, you are ${faction}`;
-
-const namee = prompt('Anna nimesi:');
-const num = Math.floor(Math.random() * 4);
-
-let faction = null;
-switch (num) {
-    case 0:
-        faction = 'Gryffindor';
-        break;
-
-    case 1:
-        faction = 'Slytherin';
-        break;
-
-    case 2:
-        faction = 'Hufflepuff';
-        break;
-
-    case 3:
-        faction = 'Ravenclaw';
-        break;
-
-    default:
-        faction = 'Factionless';
-        break;
-};
-
-document.querySelector('#target').innerText = `${namee}, you are ${faction}`;
