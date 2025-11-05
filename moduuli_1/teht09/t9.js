87b54ded8ff0f2aa21faa974d208e2db601d7404
BenLarry <4muki@gmail.com>
2025-11-05 10:44:11 +0200

    tehtävä 9 ratkaisu

--- moduuli_1/teht09/t9.js
+++ moduuli_1/teht09/t9.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const num = +prompt('Enter a number');
+
+let isPrime = true;
+
+if (num > 1) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      isPrime = false;
+    }
+  }
+} else {
+  isPrime = false;
+}
+
+if (isPrime) {
+  document.querySelector('#target').innerText = `${num} is prime number`;
+} else {
+  document.querySelector('#target').innerText = `${num} is not prime number`;
+}
