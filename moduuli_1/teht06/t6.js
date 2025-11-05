86b3806c19824cf57a1de3a2a1912bd6eca9fed3
BenLarry <4muki@gmail.com>
2025-11-05 09:32:03 +0200

    tehtävä 6 ratkaisu

--- moduuli_1/teht06/t6.js
+++ moduuli_1/teht06/t6.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const calculate = confirm('Should I calculate the square root?');
+
+if (calculate) {
+  const num = +prompt('Enter number:');
+  if (num >= 0) {
+    document.querySelector(
+      '#target'
+    ).innerText = `square root of ${num} is ${Math.sqrt(num)}`;
+  } else {
+    document.querySelector('#target').innerText =
+      'The square root of a negative number is not defined';
+  }
+} else {
+  document.querySelector('#target').innerText =
+    'The square root is not calculated.';
+}
