968e5c738f8a104d08bb2daa84ddf432f3ac1027
BenLarry <4muki@gmail.com>
2025-11-05 11:08:57 +0200

    tehtävä 10 ratkaisu

--- moduuli_1/teht10/t10.js
+++ moduuli_1/teht10/t10.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const diceAmount = +prompt('Enter number of dices');
+const sumOfInterest = +prompt('enter sum of interest');
+
+let sums = 0;
+
+for (let i = 0; i < 10000; i++) {
+  let total = 0;
+  for (let j = 0; j < diceAmount; j++) {
+    total += Math.floor(Math.random() * 6) + 1;
+    if (total === sumOfInterest) {
+      sums++;
+    }
+  }
+}
+
+const percentage = (sums / 10000) * 100;
+
+document.querySelector(
+  '#target'
+).innerText = `${diceAmount}, sum ${sumOfInterest}, propability is about ${percentage.toFixed(
+  2
+)}%`;
