52c49af54445fb8ad30bf3d5b387527d63b0139a
BenLarry <4muki4@gmail.com>
2025-11-06 16:23:31 +0200

    tehtävä 4 ratkaisu

--- moduuli_2/teht04/t4.js
+++ moduuli_2/teht04/t4.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let num = +prompt('enter a number, zero stops asking');
+
+const nums = [];
+while (num !== 0) {
+  nums.push(num);
+  num = +prompt('enter a number, zero stops asking');
+}
+
+nums.sort((a, b) => b - a);
+for (let i = 0; i < nums.length; i++) {
+  console.log(nums[i]);
+}
