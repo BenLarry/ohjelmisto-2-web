489ad0e11aeaf352808b7ff34de49fb90b96891d
BenLarry <4muki4@gmail.com>
2025-11-06 16:14:21 +0200

    tehtävä 3 ratkaisu

--- moduuli_2/teht03/t3.js
+++ moduuli_2/teht03/t3.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const dogNames = [];
+const ul = document.querySelector('#target');
+
+for (let i = 0; i < 6; i++) {
+  dogNames.push(prompt('Enter a dog name'));
+}
+
+dogNames.sort().reverse();
+for (let i = 0; i < dogNames.length; i++) {
+  let li = document.createElement('li');
+  li.innerText = dogNames[i];
+  ul.appendChild(li);
+}
