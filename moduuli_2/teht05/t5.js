0f8fc3ca0a075d75f6c2eab0a2112a9dc14f3463
BenLarry <4muki4@gmail.com>
2025-11-06 16:35:24 +0200

    tehtävä 5 ratkaisu

--- moduuli_2/teht05/t5.js
+++ moduuli_2/teht05/t5.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let num = +prompt('Enter a number, same value closes program');
+
+const nums = [];
+while (true) {
+  if (nums.includes(num)) {
+    alert(`Number ${num} has already been given`);
+    break;
+  }
+  nums.push(num);
+  num = +prompt('Enter a number, same value closes program');
+}
+
+nums.sort((a, b) => a - b);
+for (let i = 0; i < nums.length; i++) {
+  console.log(nums[i]);
+}
