9b4423b6f37d70a63d0402f81882f90c9acc3aaa
BenLarry <4muki4@gmail.com>
2025-11-06 09:39:16 +0200

    tehtävä 2 ratkaisu

--- moduuli_2/teht02/t2.js
+++ moduuli_2/teht02/t2.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ol = document.querySelector('#target');
+const participants = +prompt("Enter number of participant");
+
+const names = [];
+for(let i = 0; i < participants; i++) {
+    names.push(prompt("Enter parcipant name"));
+};
+
+names.sort();
+for(let i = 0; i < names.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = names[i];
+    ol.appendChild(li)
+};
