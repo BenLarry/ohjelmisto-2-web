7ffeba2898c9acefd9e52d3f4b758ebc1a862de1
BenLarry <4muki@gmail.com>
2025-11-07 12:47:41 +0200

    tehtävä 7 ratkaisu

--- moduuli_2/teht07/t7.js
+++ moduuli_2/teht07/t7.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const ul = document.querySelector('#target');
+const num = +prompt('enter how many sided die you want to be thrown');
+
+function diceRoll(sides) {
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+let roll = null;
+
+while (roll !== num) {
+  roll = diceRoll(num);
+  let li = document.createElement('li');
+  li.innerText = roll;
+  ul.appendChild(li);
+}
