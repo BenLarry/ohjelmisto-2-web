4053d2bfbb131604fc69fb58aeb450b164d5629e
BenLarry <4muki@gmail.com>
2025-11-07 12:55:51 +0200

    tehtävä 8 ratkaisu

--- moduuli_2/teht08/t8.js
+++ moduuli_2/teht08/t8.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const p = document.querySelector('#target');
+const names = ['Ben', 'Max', 'Charles', 'Samuli', 'Nico'];
+
+function concat(arrStrings) {
+  let concattedStrings = '';
+  for (let i = 0; i < arrStrings.length; i++) {
+    concattedStrings += arrStrings[i];
+  }
+  return (p.innerText = concattedStrings);
+}
+
+concat(names);
