022ba4ab1d1d5e6f435778e545513551806822d5
BenLarry <4muki@gmail.com>
2025-11-07 12:43:53 +0200

    tehtävä 6 ratkaisu

--- moduuli_2/teht06/t6.js
+++ moduuli_2/teht06/t6.js
@@ -1,6 +1,16 @@
 'use strict';
 
-/*Write a function that returns a random dice roll between 1 and 6. 
-The function should not have any parameters.
- Write a main program that rolls the dice until the result is 6. 
- The main program should print out the result of each roll in an unordered list (<ul>). (2p)*/
+const ul = document.querySelector('#target');
+console.log(ul);
+function diceRoll() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+let roll = null;
+
+while (roll !== 6) {
+  roll = diceRoll();
+  let li = document.createElement('li');
+  li.innerText = roll;
+  ul.appendChild(li);
+}
