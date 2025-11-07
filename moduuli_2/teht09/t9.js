3ebe01a32e6427f06c2ff209781f380bad044054
BenLarry <4muki@gmail.com>
2025-11-07 13:04:09 +0200

    tehtävä 9 ratkaisu

--- moduuli_2/teht09/t9.js
+++ moduuli_2/teht09/t9.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function even(arrNums) {
+  const evenNums = [];
+  for (let i = 0; i < arrNums.length; i++) {
+    if (arrNums[i] % 2 === 0) {
+      evenNums.push(arrNums[i]);
+    }
+  }
+  return evenNums;
+}
+
+console.log('Even numbers: ' + even(nums));
+console.log('All numbers: ' + nums);
