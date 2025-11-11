82727f462521eadfc0494fa283212dd4cf885435
BenLarry <4muki@gmail.com>
2025-11-11 14:12:32 +0200

    tehtävä 10 ratkaisu

---
+++ moduuli_3/teht10/t10.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const p = document.querySelector('#target');
+const form = document.querySelector('#source');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const names = document.querySelectorAll('form > [name]');
+  const firstName = names[0].value;
+  const lastName = names[1].value;
+  if (firstName === '' || lastName === '') {
+    return (p.textContent = 'Enter valid name');
+  }
+
+  p.textContent = `Your name is ${firstName} ${lastName}`;
+});
