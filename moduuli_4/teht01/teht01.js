3e7b2c75c323bee04d9b3604659aa3db18b5f0c3
BenLarry <4muki@gmail.com>
2025-11-12 10:40:20 +0200

    moduuli 4 tehtävä 1 ratkaisu

--- moduuli_4/teht01/teht01.js
+++ moduuli_4/teht01/teht01.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const form = document.querySelector('form');
+const query = document.querySelector('input[name=q]');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
+
+  try {
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${query.value}`
+    );
+    if (!response.ok) {
+      throw new Error('XD');
+    }
+
+    console.log(await response.json());
+  } catch (error) {
+    console.log(error.message);
+  }
 });
